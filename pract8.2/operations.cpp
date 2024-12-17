3a3402129d145ed7c69e7adcfeee89ba63287126
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2024-12-18 01:45:16 +0300

    Create operations.cpp

---
+++ pract8.2/operations.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+bool isValid(const vector<int>& sequence) {
+    for (size_t i = 1; i < sequence.size(); i++) {
+        if (sequence[i] == 1 && sequence[i - 1] == 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int bruteForceCount(int n, int& bruteForceOperations) {
+    int total = 0;
+    int combinations = pow(2, n);
+
+    for (int i = 0; i < combinations; ++i) {
+        vector<int> sequence;
+        for (int j = 0; j < n; ++j) {
+            sequence.push_back((i >> j) & 1);
+        }
+        bruteForceOperations++;
+        if (isValid(sequence)) {
+            total++;
+        }
+    }
+    return total;
+}
+
+int countSequences(int n, int& dpOperations) {
+    if (n == 1) return 2;
+    if (n == 2) return 3;
+
+    int dp_0 = 1, dp_1 = 1;
+    int new_dp_0, new_dp_1;
+
+    for (int i = 2; i <= n; i++) {
+        new_dp_0 = dp_0 + dp_1;
+        new_dp_1 = dp_0;
+
+        dp_0 = new_dp_0;
+        dp_1 = new_dp_1;
+
+        dpOperations += 2;
+    }
+
+    return dp_0 + dp_1;
+}
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    cout << "Введите длину последовательности n: ";
+    cin >> n;
+
+    int bfOperations = 0;
+    int dpOperations = 0;
+
+    int resultDP = countSequences(n, dpOperations);
+    cout << "Динамическое программирование:\n";
+    cout << "Количество последовательностей: " << resultDP << endl;
+    cout << "Количество операций: " << dpOperations << endl;
+
+    int resultBF = bruteForceCount(n, bfOperations);
+    cout << "\nПолный перебор:\n";
+    cout << "Количество последовательностей: " << resultBF << endl;
+    cout << "Количество операций: " << bfOperations << endl;
+
+    return 0;
+}
