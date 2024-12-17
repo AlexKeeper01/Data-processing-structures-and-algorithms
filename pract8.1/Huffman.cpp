40fff897f122ee631ee6d914b4b4f5ae675e5fd3
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2024-12-17 09:23:46 +0300

    Update Huffman.cpp

--- pract8.1/Huffman.cpp
+++ pract8.1/Huffman.cpp
@@ -16,39 +16,17 @@ struct Symbol {
 };
 
 struct Node {
+    int pos;
     char character;
     int frequency;
     Node* left;
     Node* right;
 
-    Node(char ch, int freq) : character(ch), frequency(freq), left(nullptr), right(nullptr) {}
+    Node(char ch, int freq, int p) : character(ch), frequency(freq), left(nullptr), right(nullptr), pos(p) {}
 };
 
-Node* buildHuffmanTree(vector<Symbol>& symbols) {
-    vector<Node*> nodes;
-    for (const auto& symbol : symbols) {
-        nodes.push_back(new Node(symbol.character, symbol.frequency));
-    }
-
-    while (nodes.size() > 1) {
-        sort(nodes.begin(), nodes.end(), [](Node* a, Node* b) {return a->frequency < b->frequency;});
-
-        Node* left = nodes[0];
-        Node* right = nodes[1];
-
-        Node* combined = new Node('\0', left->frequency + right->frequency);
-        combined->left = left;
-        combined->right = right;
-
-        nodes.erase(nodes.begin(), nodes.begin() + 2);
-        nodes.push_back(combined);
-    }
-
-    return nodes[0];
-}
-
 // Обход дерева для назначения кодов символам
-void assignCodes(Node* root, unordered_map<char, string>& huffmanCodes, string code = "") {
+void assignCodes(Node* root, unordered_map<char, string>& huffmanCodes, string code="") {
     if (!root) return;
 
     // Если это лист, сохранить код
@@ -56,6 +34,7 @@ void assignCodes(Node* root, unordered_map<char, string>& huffmanCodes, string c
         huffmanCodes[root->character] = code;
     }
 
+    
     assignCodes(root->left, huffmanCodes, code + "0");
     assignCodes(root->right, huffmanCodes, code + "1");
 }
@@ -69,14 +48,59 @@ string compressString(const string& input, const unordered_map<char, string>& hu
     return compressed;
 }
 
-void printTableWithCodes(vector<Symbol>& symbols, const unordered_map<char, string>& huffmanCodes) {
+string buildHuffmanTree(const string& input, vector<Symbol>& symbols) {
+    vector<Node*> nodes;
+    for (const auto& symbol : symbols) {
+        nodes.push_back(new Node(symbol.character, symbol.frequency, 0));
+    }
+
+    sort(nodes.begin(), nodes.end(), [](Node* a, Node* b) { return a->frequency < b->frequency; });
+
+    int i = 0;
+    for (auto& node : nodes) {
+        node->pos = i++;
+    }
+
+    while (nodes.size() > 1) {
+        sort(nodes.begin(), nodes.end(), [](Node* a, Node* b) {
+            if (a->frequency == b->frequency)
+                return a->pos < b->pos;
+            return a->frequency < b->frequency;
+            });
+        Node* left = nodes[0];
+        Node* right = nodes[1];
+
+        Node* combined = new Node('\0', left->frequency + right->frequency, 0);
+        combined->left = left;
+        combined->right = right;
+        if (left->frequency >= right->frequency) {
+            combined->pos = left->pos;
+        } else {
+            combined->pos = right->pos;
+        }
+
+        nodes.erase(nodes.begin(), nodes.begin() + 2);
+        nodes.push_back(combined);
+    }
+
+    unordered_map<char, string> huffmanCodes;
+    assignCodes(nodes[0], huffmanCodes);
+    for (auto& symbol : symbols) {
+        symbol.code = huffmanCodes.at(symbol.character);
+    }
+    return compressString(input, huffmanCodes);
+}
+
+void printTableWithCodes(vector<Symbol>& symbols) {
     cout << setw(9) << "Символ" << setw(11) << "Частота" << setw(16) << "Вероятность" << setw(20) << "Код" << endl;
     cout << string(60, '-') << endl;
 
-    sort(symbols.begin(), symbols.end(), [&](const Symbol& a, const Symbol& b) {return a.probability > b.probability; });
+    sort(symbols.begin(), symbols.end(), [&](const Symbol& a, const Symbol& b) {
+        if (a.probability == b.probability)
+            return (a.code).size() < (b.code).size();
+        return a.probability > b.probability; });
 
     for (auto& symbol : symbols) {
-        symbol.code = huffmanCodes.at(symbol.character);
         cout << setw(5) << "'" << symbol.character << "'"
             << setw(10) << symbol.frequency
             << setw(15) << symbol.probability
@@ -107,23 +131,15 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    string input = "попов алексей александрович";
+    string input = "пупкин василий кириллович";
 
     vector<Symbol> symbols = calculateFrequencies(input);
 
-    Node* root = buildHuffmanTree(symbols);
+    string encodedText = buildHuffmanTree(input, symbols);
 
-    // Назначить коды символам
-    unordered_map<char, string> huffmanCodes;
-    assignCodes(root, huffmanCodes);
+    printTableWithCodes(symbols);
 
-    // Сжать строку
-    string compressed = compressString(input, huffmanCodes);
-
-    // Вывести таблицу символов с кодами
-    printTableWithCodes(symbols, huffmanCodes);
-
-    cout << "\nСжатая строка: " << compressed << endl;
+    cout << "\nСжатая строка: " << encodedText << endl;
 
     return 0;
 }
